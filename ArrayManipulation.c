4eb941de5306241e1da37827a4428980eb22dfca
Suleman Dawood <sdawood1214@gmail.com>
2018-03-27 18:09:48 -0400

    Array Manipulation in C Example

---
+++ ArrayManipulation.c
@@ -0,0 +1,33 @@
+/* This program demonstrates a function call that takes in ana array as a parameter
+Written by: Suleman Dawood
+Date: 03/37/2018
+
+*/
+
+
+
+#include <stdio.h>
+
+void aryParam(int array[]);
+
+
+
+void main()
+{
+int intArray[] = {1,2,3};
+char charArray[] = {'a','b','c'};
+float fltArray[] = {1.1,1.2,1.3};
+
+printf("%d\n", intArray[0]);
+
+aryParam(intArray);
+
+}
+
+
+void aryParam(int array[] )
+{
+    printf("%d\n", array[0]);
+    
+}
+
