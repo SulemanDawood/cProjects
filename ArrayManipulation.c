c0aa0ae17ca76a10af4020e4178fb29449e9bbd6
Suleman Dawood <sdawood1214@gmail.com>
2018-03-27 18:23:12 -0400

    Update to loop and print integer array

--- ArrayManipulation.c
+++ ArrayManipulation.c
@@ -1,6 +1,6 @@
 /* This program demonstrates a function call that takes in ana array as a parameter
 Written by: Suleman Dawood
-Date: 03/37/2018
+Date: 03/27/2018
 
 */
 
@@ -18,7 +18,7 @@ int intArray[] = {1,2,3};
 char charArray[] = {'a','b','c'};
 float fltArray[] = {1.1,1.2,1.3};
 
-printf("%d\n", intArray[0]);
+//printf("%d\n", intArray[0]);
 
 aryParam(intArray);
 
@@ -27,7 +27,12 @@ aryParam(intArray);
 
 void aryParam(int array[] )
 {
-    printf("%d\n", array[0]);
+    //printf("%d\n", array[0]);
+    int i;
+    for (i=0; i<3; ++i)
+    {
+        printf("%d\n", array[i]);
+    }
     
 }
 
