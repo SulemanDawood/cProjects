dbe6fc57d76dd77b8955cf7e14121599943ef8fa
Suleman Dawood <sdawood1214@gmail.com>
2018-04-13 20:50:41 -0400

    This program demonstrates two-way selection in C

---
+++ twoWaySelection.c
@@ -0,0 +1,30 @@
+/*
+ ============================================================================
+ Name        :  twoWaySelection.c
+ Author      :  Suleman Dawood
+ Version     :  1.0
+ Copyright   : 
+ Date        :  04/13/2018
+ Description :  This program demonstrates two-way selection in C.              
+ ============================================================================
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+//  Local Declarations
+    int a;
+    int b;
+
+//  Statements
+    printf("Please enter two integers: ");
+    scanf("%d%d", &a, &b);
+
+    if (a <= b)
+        printf("%d <= %d\n", a, b);
+    else
+        printf("%d > %d", a, b);
+
+    return 0;
+}// main
