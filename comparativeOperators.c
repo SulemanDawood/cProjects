f51c88156af07f8729b7a41b872702ff0d82b077
Suleman Dawood <sdawood1214@gmail.com>
2018-04-13 20:38:15 -0400

    This program demonstrates the results of relational operators in C.

---
+++ comparativeOperators.c
@@ -0,0 +1,29 @@
+/*
+ ============================================================================
+ Name        :  comparativeOperators.c
+ Author      :  Suleman Dawood
+ Version     :  1.0
+ Copyright   : 
+ Date        :  04/13/2018
+ Description :  This program demonstrates the results of
+                relational operators in C.              
+ ============================================================================
+ */
+
+#include <stdio.h>
+
+int main (void)
+{
+//  Local Declarations
+    int a = 5;
+    int b = -3;
+
+//  Statements
+    printf(" %2d <  %2d is %2d\n", a, b, a < b);
+    printf(" %2d ==  %2d is %2d\n", a, b, a == b);
+    printf(" %2d !=  %2d is %2d\n", a, b, a != b);
+    printf(" %2d >  %2d is %2d\n", a, b, a > b);
+    printf(" %2d <=  %2d is %2d\n", a, b, a <= b);
+    printf(" %2d =>  %2d is %2d\n", a, b, a => b);
+    return 0;
+}// main
